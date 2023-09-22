89ffb3adf17af45b8582c4955a394315d54e1dca
SkyWace <simon.delcloque@hotmail.fr>
2023-09-22 16:15:59 +0200

    weee

--- index.php
+++ index.php
@@ -13,34 +13,41 @@ require_once("ConnexionBDD.php");
 </head>
 
 <body>
-    <h1>Questions Vrai ou Faux</h1>
-    <div id="question-container">
-        <!-- La question sera affichée ici -->
+    <h1 class="main-title">Squizzie</h1>
+    <div class="layout">
+        <div class="framed">
+            <div class="header-page">
+                <div id="question-container">
+                    <!-- La question sera affichée ici -->
+                </div>
+                <div class="squizzie-button" id="button-container">
+                    <button class="true" id="trueButton">Vrai</button>
+                    <button class="false" id="falseButton">Faux</button>
+                </div>
+                <div class="answer-result">
+                    <div class="result" id="result"></div>
+                </div>
+                <a href="Quizuse/creer_quiz.php">Créer un Quizz</a>
+                <!-- Options "J'aime" -->
+                <div class="like-options">
+                    <form action="Note.php" method="post">
+                        <label>
+                            <input type="range" id="ratingSlider" name="note" min="1" max="5" step="1" value="1">
+                            <span class="like-label">Note :</span>
+                            <span class="like-count" id="ratingValue">1</span>
+                            <button type="submit">Enregistrer la note</button>
+                        </label>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="button-container">
-        <button id="trueButton">Vrai</button>
-        <button id="falseButton">Faux</button>
+        
+
+    <!-- Compteur de bonnes réponses (initialement caché) -->
+    <div class="test" id="correct-answers">
+        <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
     </div>
-    <div id="result"></div>
-    <a href="Quizuse/creer_quiz.php">Créer un Quizz</a>
-    <!-- Options "J'aime" -->
-    <div class="like-options">
-    <form action="Note.php" method="post">
-        <label>
-            <input type="range" id="ratingSlider" name="note" min="1" max="5" step="1" value="1">
-            <span class="like-label">Note :</span>
-            <span class="like-count" id="ratingValue">1</span>
-            <button type="submit">Enregistrer la note</button>
-        </label>
-    </form>
-</div>
-
-
-
-<!-- Compteur de bonnes réponses (initialement caché) -->
-<div class="test" id="correct-answers">
-    <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
-</div>
 </body>
 
 </html>
