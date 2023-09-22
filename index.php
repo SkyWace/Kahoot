b62a9d8bccff4ba540ca36576cc8225c7871af4f
SkyWace <simon.delcloque@hotmail.fr>
2023-09-22 15:57:14 +0200

    NOTEEEE

--- index.php
+++ index.php
@@ -13,58 +13,34 @@ require_once("ConnexionBDD.php");
 </head>
 
 <body>
-    <h1 class="main-title">Squizzie</h1>
-    <div class="layout">
-        <div class="framed">
-            <div class="header-page">
-                <div id="question-container">
-                    <!-- La question sera affichée ici -->
-                </div>
-                <div class="squizzie-button" id="button-container">
-                    <button class="true" id="trueButton">Vrai</button>
-                    <button class="false" id="falseButton">Faux</button>
-                </div>
-                <div class="answer-result">
-                    <p class="result" id="result"></p>
-                </div>
-
-                <a href="Quizuse/creer_quiz.php">Créer un Quizz</a>
-
-                <!-- Options "J'aime" -->
-                <div class="like-options">
-                    <label>
-                        <input type="radio" name="like-option" value="1">
-                        <span class="like-label">1 étoile</span>
-                        <span class="like-count" data-count="0">0</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="like-option" value="2">
-                        <span class="like-label">2 étoiles</span>
-                        <span class="like-count" data-count="0">0</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="like-option" value="3">
-                        <span class="like-label">3 étoiles</span>
-                        <span class="like-count" data-count="0">0</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="like-option" value="4">
-                        <span class="like-label">4 étoiles</span>
-                        <span class="like-count" data-count="0">0</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="like-option" value="5">
-                        <span class="like-label">5 étoiles</span>
-                        <span class="like-count" data-count="0">0</span>
-                    </label>
-                </div>
-            </div>
-        </div>
+    <h1>Questions Vrai ou Faux</h1>
+    <div id="question-container">
+        <!-- La question sera affichée ici -->
     </div>
-    <!-- Compteur de bonnes réponses (initialement caché) -->
-    <div class="test" id="correct-answers">
-        <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
+    <div id="button-container">
+        <button id="trueButton">Vrai</button>
+        <button id="falseButton">Faux</button>
     </div>
+    <div id="result"></div>
+    <a href="Quizuse/creer_quiz.php">Créer un Quizz</a>
+    <!-- Options "J'aime" -->
+    <div class="like-options">
+    <form action="Note.php" method="post">
+        <label>
+            <input type="range" id="ratingSlider" name="note" min="1" max="5" step="1" value="1">
+            <span class="like-label">Note :</span>
+            <span class="like-count" id="ratingValue">1</span>
+            <button type="submit">Enregistrer la note</button>
+        </label>
+    </form>
+</div>
+
+
+
+<!-- Compteur de bonnes réponses (initialement caché) -->
+<div class="test" id="correct-answers">
+    <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
+</div>
 </body>
 
 </html>
