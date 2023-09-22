879179d1e39e315d6214a1a2ffce52e1c1f0d2af
SkyWace <simon.delcloque@hotmail.fr>
2023-09-22 09:23:00 +0200

    Ancien pro

---
+++ question.php
@@ -0,0 +1,13 @@
+<?php
+$questions = [
+    "question1" => [
+        "text" => "La Terre est plate.",
+        "answer" => false
+    ],
+    "question2" => [
+        "text" => "Le soleil tourne autour de la Terre.",
+        "answer" => false
+    ],
+    // Ajoutez d'autres questions ici
+];
+?>
