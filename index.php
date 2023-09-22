aaeb5f17f3f64256a29445c6ffdeff0d55afcae1
SkyWace <simon.delcloque@hotmail.fr>
2023-09-22 15:57:14 +0200

    NOTEEEE

--- index.php
+++ index.php
@@ -42,12 +42,30 @@ require_once("ConnexionBDD.php");
             </div>
         </div>
     </div>
-        
-
-    <!-- Compteur de bonnes réponses (initialement caché) -->
-    <div class="test" id="correct-answers">
-        <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
+    <div id="button-container">
+        <button id="trueButton">Vrai</button>
+        <button id="falseButton">Faux</button>
     </div>
+    <div id="result"></div>
+    <a href="Quizuse/creer_quiz.php">Créer un Quizz</a>
+    <!-- Options "J'aime" -->
+    <div class="like-options">
+    <form action="Note.php" method="post">
+        <label>
+            <input type="range" id="ratingSlider" name="note" min="1" max="5" step="1" value="1">
+            <span class="like-label">Note :</span>
+            <span class="like-count" id="ratingValue">1</span>
+            <button type="submit">Enregistrer la note</button>
+        </label>
+    </form>
+</div>
+
+
+
+<!-- Compteur de bonnes réponses (initialement caché) -->
+<div class="test" id="correct-answers">
+    <p>Nombre de bonnes réponses : <span id="correct-answer-count">0</span></p>
+</div>
 </body>
 
 </html>
